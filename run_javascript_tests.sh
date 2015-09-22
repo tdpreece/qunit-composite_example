b1ff3f2821e4cd90e61e7261c44994d1eda64cf9
Tim Preece <tdpreece@gmail.com>
2015-09-22 20:55:10 +0100

    Added example of used qunit_composite.

---
+++ run_javascript_tests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+phantomjs /usr/local/lib/node_modules/qunit-phantomjs-runner/runner.js all_tests.html
---
+++ test1.js
@@ -0,0 +1,3 @@
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 2 == "1", "Passed!" );
+});
